906097976ea11020ba294524bb350ff8c4cafb5e
Patrick <p@rickjobin.com>
2019-06-26 19:27:02 -0400

    fixed localhost issue

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('click', (e) => {
 
     messageOne.textContent = 'Loading...'
 
-    fetch('http://localhost:3000/page').then((response) => {
+    fetch('/page').then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log('sssss')
